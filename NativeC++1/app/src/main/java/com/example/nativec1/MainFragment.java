e5b6e296ab02147a50c1e095074f5552e88cc9ab
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-15 13:16:24 +0900

    現状でもシステムは動く

---
+++ NativeC++1/app/src/main/java/com/example/nativec1/MainFragment.java
@@ -0,0 +1,21 @@
+package com.example.nativec1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
